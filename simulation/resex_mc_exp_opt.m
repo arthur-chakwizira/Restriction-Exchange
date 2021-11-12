efd827545b5619798d43fa979b1e9b920b831267
arthur-chakwizira <60353512+arthur-chakwizira@users.noreply.github.com>
2021-11-12 14:59:29 +0100

    Delete simulation directory

--- simulation/resex_mc_exp_opt.m
+++
@@ -1,5 +0,0 @@
-function opt = resex_mc_exp_opt()
-opt.exp_opt.g_max = 80e-3;
-opt.exp_opt.slew_max = 70;
-opt.exp_opt.using_sim_data = true;
-end
--- simulation/resex_mc_one_circle_restrict_particles.m
+++
@@ -1,9 +0,0 @@
-function     R_new = resex_mc_one_circle_restrict_particles(R_new, R_old, r)
-%restrict particles to a circle of radius r
-
-escaped =  ( (R_new(:,1)).^2 + (R_new(:,2)).^2  ) >= r^2;
-
-
-R_new(escaped, :) = R_old(escaped, :);
-
-end
